dcef22aadd150abf9ae9c89fa3dadbb55c5cfcf0
unknown <mixankan9@gmail.com>
2021-02-02 11:07:34 +0600

    double and triple equal

--- undefined.js
+++ undefined.js
@@ -1,8 +1,4 @@
-const premik = {name = "smart dude" , phone = 1234567};
-console.log(premik);
-
-let pakhi;
-console.log(pakhi);
-
-let fun = undefined;
-console.log(ages[11]);
+function doSomething(x,y){
+    console.log(y);
+}
+doSomething(32);
