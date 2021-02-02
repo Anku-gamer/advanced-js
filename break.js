979022f3a7484837eb9c0d05faf7de002e2d2227
unknown <mixankan9@gmail.com>
2021-02-02 12:39:52 +0600

    break

--- break.js
+++ break.js
@@ -4,11 +4,6 @@ for(let i=0; i < nums.length; i++){
     if(nums[i]>3){
         break;
     }
-    // console.log(nums[i]);
+ console.log(nums[i]);
 }
 
-for(let i = 0; i < nums.length; i++){
-   
-    if(nums[i] < 0){
-        continue;
-    }
